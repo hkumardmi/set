1d6794ceeb7a515a7de0adb081fff5af7518db09
Himanshu <hkumar@dminc.com>
2020-01-23 12:17:39 +0530

    hey chapter new

---
+++ javascript/exjs1.js
@@ -0,0 +1,33 @@
+        var x = prompt('Enter Your Age ');
+        if (x <= 18){
+            console.log('Not elizible for Vote');
+        }
+        else if (x>=60) {
+            console.log('you can not wait')
+        }
+        else{
+            console.log('Register for Vote Today');
+        }
+       
+
+        var x = prompt('Enter any under 3 ');
+        switch(x) {
+                case 1:
+                    day = "Sunday";
+                    break;
+                case (2):
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;
+                default :
+                    day =  'Today is not any day.';
+                }
+        console.log(day);
+        
+        var arr = [1,2,3,4,5,6,7,8,9];
+        console.log(arr);
+        for (i = 0; i < arr.length; i++) {
+            console.log(arr[i])
+          }
